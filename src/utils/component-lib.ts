c85fda4a7d8b9cfa2bcad6c2961e0dd5ee293d92
George Kemp <gkemp@hubspot.com>
2020-10-27 11:14:02 -0600

    Fix Files & Bug Fixes

--- src/utils/component-lib.ts
+++ src/utils/component-lib.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export const safelyGetComponent = (component: string) => {
   try {
-    return AdminBro.bundle(path.resolve(__dirname, `../components/${component}`))
+    return AdminBro.bundle(path.resolve(__dirname, `../../components/${component}`))
   } catch (e) {
     return undefined;
   }
